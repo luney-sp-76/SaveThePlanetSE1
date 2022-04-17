27a1903a6120d059a5e15ee5ef8ed2bb1127867b
Paul Olphert <69307367+luney-sp-76@users.noreply.github.com>
2022-04-17 18:26:24 +0100

    Update Testing intelliJ

---
+++ src/com.savetheplanet.Main/com/savetheplanet/test/PlayerTest.java
@@ -0,0 +1,30 @@
+package com.savetheplanet.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPoints() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setPoints() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
