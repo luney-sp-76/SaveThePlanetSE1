b4d1f8e1912fb484268858734552426b3980e4f4
ravus <jaszon@gmail.com>
2022-04-19 10:40:05 +0100

    Just testing branch push

--- src/com.savetheplanet.Main/Driver.java
+++ src/com.savetheplanet.Main/Driver.java
@@ -1,11 +1,13 @@
 package com.savetheplanet.Main;
 
+// testing branch
+
 public class Driver {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         try {
-            Player p1 = new Player("Dog");
+            Player p1 = new Player("");
 
             System.out.println(p1.toString());
 
