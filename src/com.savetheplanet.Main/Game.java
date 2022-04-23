526f5b2a069d03bf2d1585e213f83474c52e4810
Paul Olphert <69307367+luney-sp-76@users.noreply.github.com>
2022-04-23 16:22:26 +0100

    update removes redundant driver tests implements Die in Game

--- src/com.savetheplanet.Main/Game.java
+++ src/com.savetheplanet.Main/Game.java
@@ -1,8 +1,9 @@
 package com.savetheplanet.Main;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
-public class Game {
+public class Game implements Die{
 
     private static List<Square> board = new ArrayList<>();
     private static List<Player> players = new ArrayList<>();
@@ -226,4 +227,28 @@ public class Game {
     public static void collectFunding(Player player) {
         player.setFunding((player.getFunding() + COLLECT));
     }
+}
+
+    @Override
+    public int roll() throws InterruptedException {
+        //A message is displayed saying “Dice Rolling...”
+        System.out.println("Dice Rolling...");
+        //The dice roll takes a few seconds.
+        TimeUnit.SECONDS.sleep(3);
+        int die1Result = randomNum();
+        int die2Result = randomNum();
+        int totalResult = die1Result + die2Result;
+        //A message is then displayed saying “Die 1 is x, Die 2
+        //is y. You will move forward x+y places.”
+        System.out.printf("Die 1 is %d, Die 2 is %d...%nYou will move forward %d spaces.%n", die1Result, die2Result, totalResult);
+        return totalResult;
+    }
+
+    private int randomNum(){
+        int min = 1;
+        int max = 6;
+        int result = (int)Math.floor(Math.random()*(max-min+1)+min);
+        return  result;
+    }
+
 }
