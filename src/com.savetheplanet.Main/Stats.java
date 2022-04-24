17759473403e9a7e7cd492ea9760d0395f7db38c
ravus <jaszon@gmail.com>
2022-04-24 21:00:34 +0100

    Feature - Player Stats v3. Winning player calculation and final output. The Stats class is now feature complete- for now.

--- src/com.savetheplanet.Main/Stats.java
+++ src/com.savetheplanet.Main/Stats.java
@@ -1,9 +1,12 @@
 package com.savetheplanet.Main;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.*;
 
 public class Stats {
 
@@ -20,14 +23,15 @@ public class Stats {
 
     public void full() {
         rank = new AtomicInteger(0);
+
         players.forEach(Player::calcTotalValue);
+
         System.out.println("###################################################");
         System.out.println("################## Leader Board ###################");
         System.out.println("###################################################");
         System.out.printf("| %-4s | %-32s | %-6s|%n", "Rank", "Player", "Funds");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-//        players.sort(Comparator.comparing(Player::getFunding));
         players.sort(Comparator.comparing(Player::getTotalValue).reversed());
 
         players.forEach(player -> {
@@ -43,6 +47,49 @@ public class Stats {
         System.out.println();
     }
 
+    public void end() {
+
+        full();
+        String winCondition = drawCheck();
+
+        System.out.println("oooooo   oooooo     oooo  o8o");
+        System.out.println(" `888.    `888.     .8'   `\"'");
+        System.out.println("`888.   .8888.   .8'   oooo  ooo. .oo.   ooo. .oo.    .ooooo.  oooo d8b ");
+        System.out.println("`888  .8'`888. .8'    `888  `888P\"Y88b  `888P\"Y88b  d88' `88b `888\"\"8P ");
+        System.out.println("`888.8'  `888.8'      888   888   888   888   888  888ooo888  888 ");
+        System.out.println("`888'    `888'       888   888   888   888   888  888    .o  888");
+        System.out.println("`8'      `8'       o888o o888o o888o o888o o888o `Y8bod8P' d888b");
+        System.out.println();
+        // this bold ansi escape doesn't work on every OS. Console is so limited man.
+        System.out.printf("%-6s\u001B[1m %s!%n%s.%n", "Congratulations", players.get(0).getName(), "Won by " + winCondition);
+    }
+
+    /**
+     * @return Condition player won by.
+     */
+    private String drawCheck() {
+
+        // If one player has the most total value they win outright
+        if (players.get(0).getTotalValue() > players.get(1).getTotalValue())
+            return "Total Value";
+
+        // Otherwise, remove the players with less Total Value than the winning amount.
+        players = players.stream().filter(p -> p.getTotalValue() == players.get(0).totalValue).collect(toList());
+
+        System.out.println(players);
+        // sort by funding, descending.
+        players.sort(Comparator.comparing(Player::getFunding).reversed());
+
+        // If one remaining player has the most funding they win by first tie-break rule.
+        if (players.get(0).getFunding() > players.get(1).getFunding())
+            return "Tie-break 1: Funding";
+
+        // if there is still no clear winner, they are chosen by random selection from those deadlocked.
+        Collections.shuffle(players);
+        return "Tie-Break 2: Random Selection";
+    }
+
+
     public void abr() {
         rank = new AtomicInteger(0);
         players.forEach(Player::calcTotalValue);
@@ -51,7 +98,6 @@ public class Stats {
         System.out.printf("| %-4s | %-32s | %-6s|%n", "Rank", "Player", "");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-//        players.sort(Comparator.comparing(Player::getFunding));
         players.sort(Comparator.comparing(Player::getTotalValue).reversed());
 
         players.forEach(player -> {
