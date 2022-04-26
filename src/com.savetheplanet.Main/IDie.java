39bd31568bc6508786df62cfaa20c8650d160857
Paul Olphert <69307367+luney-sp-76@users.noreply.github.com>
2022-04-26 14:00:53 +0100

    UC-01 refactors audio() to clapAudio() file in Game Class as the dice sound has migrated to the Dice Class

--- src/com.savetheplanet.Main/IDie.java
+++ src/com.savetheplanet.Main/IDie.java
@@ -1,10 +1,7 @@
 package com.savetheplanet.Main;
 
-import javax.sound.sampled.UnsupportedAudioFileException;
-import java.util.concurrent.TimeUnit;
-
 public interface IDie {
-    public void roll() throws UnsupportedAudioFileException;
+     void roll() ;
 
 }
 
